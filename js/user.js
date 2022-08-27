ea5586beb6db236b97b37657a917bf32e9cadbe9
Saqib Ahmad <you@example.com>
2022-08-27 07:53:53 +0600

    users dynamically added by js

--- js/user.js
+++ js/user.js
@@ -1,5 +1,17 @@
-function loadData(){
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(user => loadUsers(user))
+}
+
+function loadUsers(users) {
+    const userList = document.getElementById('user-list');
+    for (let user of users) {
+        const userName = user.name
+        console.log(user.name)
+        const li = document.createElement('li');
+        li.innerText = `${userName}`
+        userList.appendChild(li);
+    }
+
 }
